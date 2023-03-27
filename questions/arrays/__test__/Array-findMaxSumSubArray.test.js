702f7cc18e7c93799c1899dc3c8d146e02752e61
Rajesh <rajeshpradhan04@gmail.com>
2023-03-27 20:52:45 +0530

    update

---
+++ questions/arrays/__test__/Array-findMaxSumSubArray.test.js
@@ -0,0 +1,22 @@
+const code = require("../findMaxSumSubArray");
+
+describe("findMaxSumSubArray", () => {
+    test("should work", () => {
+        expect(code.findMaxSumSubArrayV1([-4, 2, -5, 1, 2, 3, 6, -5, 1])).toBe(
+            12
+        );
+        expect(code.findMaxSumSubArrayV1([-2, 10, 7, -5, 15, 6])).toBe(33);
+    });
+    test("should work", () => {
+        expect(code.findMaxSumSubArrayV2([-4, 2, -5, 1, 2, 3, 6, -5, 1])).toBe(
+            12
+        );
+        expect(code.findMaxSumSubArrayV2([-2, 10, 7, -5, 15, 6])).toBe(33);
+    });
+    test("should work", () => {
+        expect(code.findMaxSumSubArrayV3([-4, 2, -5, 1, 2, 3, 6, -5, 1])).toBe(
+            12
+        );
+        expect(code.findMaxSumSubArrayV3([-2, 10, 7, -5, 15, 6])).toBe(33);
+    });
+});
