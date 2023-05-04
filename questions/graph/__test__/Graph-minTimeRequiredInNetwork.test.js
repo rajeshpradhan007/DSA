2430bc3150f7ca59957f570d0442cc78edea717d
Rajesh <rajeshpradhan04@gmail.com>
2023-05-04 23:59:37 +0530

    graph

---
+++ questions/graph/__test__/Graph-minTimeRequiredInNetwork.test.js
@@ -0,0 +1,8 @@
+const { minTimeRequiredInNetworkV1 } = require("../Graph-minTimeRequiredInNetwork");
+
+describe("findMaxSumSubArray", () => {
+    test("should work", () => {
+        let arr=[[1,2,9],[1,4,2],[2,5,1],[4,2,4],[4,5,6],[3,2,3],[5,3,7],[3,1,5]];
+        minTimeRequiredInNetworkV1(arr);
+    });
+});
